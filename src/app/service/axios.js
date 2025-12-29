3362b769d70afafc62e0e1ffb53a88af8678f23e
Maryam Mudasiru <mm5adeniky@gmail.com>
2025-12-29 08:18:18 +0100

    refactor: update API base URL to use NEXT_PUBLIC_BASE_URL and simplify payment initialization

--- src/app/service/axios.js
+++ src/app/service/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { setupInterceptorsTo } from "./interceptors";
 
 const client = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
 });
 
 export const Api = setupInterceptorsTo(client);
